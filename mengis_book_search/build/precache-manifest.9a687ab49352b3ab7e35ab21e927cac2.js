c3f6a17ebaf39bccc55094a2aece12d0c0c64ccc
LJFrancis <joe.mengis@gmail.com>
2020-10-14 22:02:54 -0700

    add build

---
+++ mengis_book_search/build/precache-manifest.9a687ab49352b3ab7e35ab21e927cac2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca64136c3a1a4ffb75dab24aba69fd31",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b2cae2acbe976ada2367",
+    "url": "./static/css/2.e2e9731f.chunk.css"
+  },
+  {
+    "revision": "4f90fc72432257fa5c88",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b2cae2acbe976ada2367",
+    "url": "./static/js/2.4f71059f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.4f71059f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f90fc72432257fa5c88",
+    "url": "./static/js/main.f34cccd9.chunk.js"
+  },
+  {
+    "revision": "803885f2f8f9d4ff8da8",
+    "url": "./static/js/runtime-main.8da7ff2a.js"
+  },
+  {
+    "revision": "ca3c1854a891e1482a2cc254711b3897",
+    "url": "./static/media/Mengis_Books_Logo.ca3c1854.png"
+  }
+]);
