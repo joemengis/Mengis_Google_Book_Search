a6000a0c1c498ca71dc9a6133626ecf8b5d05e5c
LJFrancis <joe.mengis@gmail.com>
2020-10-14 18:07:41 -0700

    add heroku port

--- mengis_book_search/src/components/Logo.jsx
+++ mengis_book_search/src/components/Logo.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
-import logo from '../logo.svg'
+import logo from '../features/images/Mengis_Books_Logo.png'
 
 const Wrapper = styled.a.attrs({
     className: 'navbar-brand',
@@ -11,7 +11,9 @@ class Logo extends Component {
     render() {
         return (
             <Wrapper href="https://sambarros.com">
+                <a href="/books/search">
                 <img src={logo} width="50" height="50" alt="sambarros.com" />
+                </a>
             </Wrapper>
         )
     }
